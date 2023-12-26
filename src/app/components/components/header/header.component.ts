d9c4950583d87e426d034cf27cdfae7d95985794
Zura014 <zuragagnidze50@gmail.com>
2023-12-26 15:46:58 +0400

    header-halfway

--- src/app/components/components/header/header.component.ts
+++ src/app/components/components/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isClickedBM: boolean = false;
 
+  toggleBM(){
+    this.isClickedBM =!this.isClickedBM;
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1430px",
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["Noto Sans Georgian", "sans-serif"],
+      },
+    },
+  },
+  plugins: [ 
+  ],
+}
