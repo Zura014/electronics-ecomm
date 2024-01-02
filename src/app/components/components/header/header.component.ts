18e9a9b8673b540ae09650fa34cef12950d326a0
Zura014 <zuragagnidze50@gmail.com>
2024-01-02 17:50:08 +0400

    header UI

--- src/app/components/components/header/header.component.ts
+++ src/app/components/components/header/header.component.ts
@@ -7,7 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isClickedBM: boolean = false;
+  isClickedSearch: boolean = false;
+  bme: boolean = false;
+  bmeh: boolean = false;
+  bmec: boolean = false;
+  bmem: boolean = false;
+  
 
+  toggleBmem(): void { 
+    this.bmem = !this.bmem;
+  }
+  toggleBmec(): void { 
+    this.bmec = !this.bmec;
+  }
+  toggleBmeh(): void { 
+    this.bmeh = !this.bmeh;
+  }
+  toggleBme(): void { 
+    this.bme = !this.bme;
+  }
+  toggleSearch(){
+    this.isClickedSearch = !this.isClickedSearch;
+  }
   toggleBM(){
     this.isClickedBM =!this.isClickedBM;
   }
